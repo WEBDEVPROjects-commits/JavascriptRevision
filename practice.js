410f19ad798cc80c66fc1e766730c9bf7d4dc98f
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-01-10 18:09:17 +0530

    Console Log

---
+++ practice.js
@@ -0,0 +1,8 @@
+document.title="Console log"
+var a=prompt("Please enter your age");
+if(a>=18){
+    console.log("You are eligible for driving");
+}
+else{
+    console.log("You are not eligible for driving");
+}
