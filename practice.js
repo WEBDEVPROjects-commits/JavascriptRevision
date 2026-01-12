d03c33b3bc37d4abcbfaade4a428d35eb8e6b93b
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-01-12 19:45:59 +0530

    Strings,ItsFunctions

--- practice.js
+++ practice.js
@@ -1,42 +1,14 @@
-//Write a program to print the marks of a student in an object using for in loop
-
-const stud={
-    harry:98,
-    rohan:70,
-    akash:7
-}
- var names=Object.keys(stud);
-for(var i=0;i< names.length;i++){
-   console.log(stud[names[i]]);
-}
-
-//print try again until the user enters the correct number
-// var Cornumber=1;
-// var Usernumber;
-// while(1){
-//     Usernumber=Number(prompt("Please enter your guessed number"));
-//     switch(Usernumber){
-//         case Cornumber:
-//             console.log("Your guess is correct!");
-//             break;
-//         default:
-//             console.log("Try again!");
-//     }
-//     if(Usernumber===Cornumber){
-//         break;
-//     }
-// }
-//Write a function to find mean of 5 numbers
-var a=10;
-var b=32;
-var c=41;
-var d=68;
-var e=83;
-function mean(a,b,c,d,e){
-    return (a+b+c+d+e)/5;
-}
-var mean2=(a,b,c,d,e)=>{
-    return (a+b+c+d+e)/5;
-}   
-console.log(mean(a,b,c,d,e));
-console.log(mean2(a,b,c,d,e));
+//Strings
+var a='Harshit';
+var b='bhatt';
+console.log("hey i am "+a);
+console.log("Hey i am",a);
+console.log(a.trim());
+console.log(a.replace("sh","oh"));
+console.log(a.concat(b+ "yes"));
+console.log(a.includes("ti"));
+//extract amount of the string "Please give me rs 1000"
+var amount="Please give me rs 1000"
+console.log(amount.slice(18));
+//try to change fourth character of the string
+console.log(amount.replace(amount.charAt(3),'x'))
