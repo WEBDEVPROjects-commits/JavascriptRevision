d550690469e2aeacf7d63bc842bca91c9c99d4f4
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-01-12 20:33:42 +0530

    Faulty Calculator

--- practice.js
+++ practice.js
@@ -1,14 +1,49 @@
-//Strings
-var a='Harshit';
-var b='bhatt';
-console.log("hey i am "+a);
-console.log("Hey i am",a);
-console.log(a.trim());
-console.log(a.replace("sh","oh"));
-console.log(a.concat(b+ "yes"));
-console.log(a.includes("ti"));
-//extract amount of the string "Please give me rs 1000"
-var amount="Please give me rs 1000"
-console.log(amount.slice(18));
-//try to change fourth character of the string
-console.log(amount.replace(amount.charAt(3),'x'))
+/*Create a faulty calculator using javascript
+
+This faulty calculator does following:
+1.It take two numbers as input from the user
+2.It performs wrong operations as follows:
+
++ -----> -
+* -----> +
+- -----> /
+/ -----> **
+
+
+And it performs wrong operations only 10% of the times
+*/
+var op1=Number(prompt("Please enter first number"));
+var op2=Number(prompt("Please enter second number"));
+var operand=prompt("enter the operation(+,*,-,/) that you wish to perform")
+
+function calculator (op1,op2){
+    if(Math.random()<=0.1){
+        if(operand=='+'){
+            return op1-op2
+        }
+        else if(operand=='*'){
+            return op1+op2
+        }
+        else if(operand=="-"){
+            return op1/op2
+        }
+        else if(operand=="/"){
+            return op1**op2
+        }
+    }
+    else{
+         if(operand=='+'){
+            return op1+op2
+        }
+        else if(operand=='*'){
+            return op1*op2
+        }
+        else if(operand=="-"){
+            return op1-op2
+        }
+        else if(operand=="/"){
+            return op1/op2
+        }
+    }
+}
+console.log(`Your result is ${calculator(op1,op2)}`)
