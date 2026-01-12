8f6a79cdc977e71e6bf05b9c40984ed278b17b78
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-01-12 13:09:40 +0530

    Switches in js

--- practice.js
+++ practice.js
@@ -1,11 +1,12 @@
-//  making a 5 word dictionary using a button.
-var dictionary={
-    Happy:"Feeling Pleasure",
-    Big:"Large in size",
-    Run:"Move quickly on foot",
-    Good:"Satisfactory",
-    See:"To notice with your eyes"
-}
-for(let key in dictionary){
-    console.log(dictionary[key]);
+//  making a 5 word dictionary using a button.\
+var day="monday";
+switch(day){
+    case "monday":
+        console.log("It's monday folks!");
+        break;
+    case "tuesday":
+        console.log("It's tuesday folks!")
+        break;
+    default:
+        console.log("couldn't find any day");
 }
