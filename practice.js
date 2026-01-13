47e2978d22c8cd84c594c6211611a667f32c63d0
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-01-13 17:06:15 +0530

    filter,map,reduce

--- practice.js
+++ practice.js
@@ -1,18 +1,18 @@
-// Keep adding numbers to array until 0 is added to the array
-var arr = [31, 25, 60, 0, 78];
-var element,isTrue;
-while (1) {
-    isTrue = confirm("Do you wish to add an element to arr?");
-    if (isTrue) {
-        element = prompt("Please enter the element you wish to add to the array");
-        if(element==0){
-            break;
-        }
-        arr.push(element);
-    }
-    else {
-        console.log("Thanks for using this application");
-        break;
-    }
-}
-console.log('The new array is :' + arr)
+// filter for numbers divisible by 10 from a given array.
+var arr = [31, 25, 60, 0, 78,80];
+
+// newarr=arr.filter((element)=> (element%10)==0)
+// console.log(newarr);
+//Create an array of square of given numbers.
+newarr= arr.map((e)=>{
+    return e*e; 
+})
+console.log(newarr);
+//Use reduce to calculate the factorial of a given number from any array of first n natural numbers.(n being the number whose factorial needs to be calculated.)
+
+
+var natural=[1,2,3,4,5,6]
+var newArray=natural.reduce((acc,curr)=>{
+        return curr*acc;
+},1)
+console.log(newArray)
