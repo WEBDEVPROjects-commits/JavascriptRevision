f103604dac8d2af5f9ac579b267e084a48fe3d9f
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-01-13 13:00:35 +0530

    Array1

--- practice.js
+++ practice.js
@@ -1,49 +1,16 @@
-/*Create a faulty calculator using javascript
+// Create an array of numbers and take input from the user to add numbers to this array
 
-This faulty calculator does following:
-1.It take two numbers as input from the user
-2.It performs wrong operations as follows:
-
-+ -----> -
-* -----> +
-- -----> /
-/ -----> **
-
-
-And it performs wrong operations only 10% of the times
-*/
-var op1=Number(prompt("Please enter first number"));
-var op2=Number(prompt("Please enter second number"));
-var operand=prompt("enter the operation(+,*,-,/) that you wish to perform")
-
-function calculator (op1,op2){
-    if(Math.random()<=0.1){
-        if(operand=='+'){
-            return op1-op2
-        }
-        else if(operand=='*'){
-            return op1+op2
-        }
-        else if(operand=="-"){
-            return op1/op2
-        }
-        else if(operand=="/"){
-            return op1**op2
-        }
+var arr = [31, 25, 60, 0, 78];
+var element,isTrue;
+while (1) {
+    isTrue = confirm("Do you wish to add an element to arr?");
+    if (isTrue) {
+        element = prompt("Please enter the element you wish to add to the array");
+        arr.push(element);
     }
-    else{
-         if(operand=='+'){
-            return op1+op2
-        }
-        else if(operand=='*'){
-            return op1*op2
-        }
-        else if(operand=="-"){
-            return op1-op2
-        }
-        else if(operand=="/"){
-            return op1/op2
-        }
+    else {
+        console.log("Thanks for using this application");
+        break;
     }
 }
-console.log(`Your result is ${calculator(op1,op2)}`)
+console.log('The new array is :' + arr)
