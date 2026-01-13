4721ad060ad08a249dda5b8c82e1a9911ffed755
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-01-13 13:04:57 +0530

    Array2

--- practice.js
+++ practice.js
@@ -1,11 +1,13 @@
-// Create an array of numbers and take input from the user to add numbers to this array
-
+// Keep adding numbers to array until 0 is added to the array
 var arr = [31, 25, 60, 0, 78];
 var element,isTrue;
 while (1) {
     isTrue = confirm("Do you wish to add an element to arr?");
     if (isTrue) {
         element = prompt("Please enter the element you wish to add to the array");
+        if(element==0){
+            break;
+        }
         arr.push(element);
     }
     else {
