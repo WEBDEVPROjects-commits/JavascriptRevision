520c553bf01f92417424e303e4b1a4724d652f21
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-01-13 18:50:06 +0530

    DOM

--- practice.js
+++ practice.js
@@ -1,18 +1,12 @@
-// filter for numbers divisible by 10 from a given array.
-var arr = [31, 25, 60, 0, 78,80];
-
-// newarr=arr.filter((element)=> (element%10)==0)
-// console.log(newarr);
-//Create an array of square of given numbers.
-newarr= arr.map((e)=>{
-    return e*e; 
-})
-console.log(newarr);
-//Use reduce to calculate the factorial of a given number from any array of first n natural numbers.(n being the number whose factorial needs to be calculated.)
-
-
-var natural=[1,2,3,4,5,6]
-var newArray=natural.reduce((acc,curr)=>{
-        return curr*acc;
-},1)
-console.log(newArray)
+var age=-10;
+// if(age<0){
+//     // console.error("Age cannot be negative");
+//     location.href="https://www.google.com";
+// }
+// console.log("Hello");
+var isTrue=confirm("Do you want to change the background color?");
+var back;
+if(isTrue){
+    back=prompt("enter the background color");
+    document.body.style.backgroundColor=`${back}`;
+}
