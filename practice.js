fb9a34d96ba7104991ed7459333ed3bd6bd1cecd
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-01-15 16:22:02 +0530

    SelectingElementsInJs

--- practice.js
+++ practice.js
@@ -1,12 +1,3 @@
-var age=-10;
-// if(age<0){
-//     // console.error("Age cannot be negative");
-//     location.href="https://www.google.com";
-// }
-// console.log("Hello");
-var isTrue=confirm("Do you want to change the background color?");
-var back;
-if(isTrue){
-    back=prompt("enter the background color");
-    document.body.style.backgroundColor=`${back}`;
+for(value of document.querySelectorAll(".container ul li") ){
+    value.style.backgroundColor="cyan"
 }
