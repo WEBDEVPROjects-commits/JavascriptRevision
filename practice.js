a2ae6e774d3731d6f89c455bb4e0bf64c9887afa
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-01-16 13:52:02 +0530

    WebsiteBookmarker

--- practice.js
+++ practice.js
@@ -1,3 +1,9 @@
-document.querySelector(".container").insertAdjacentText("afterbegin",'<p>INSERTING THIS ELEMENT</p>')
+// document.querySelector(".box").onclick=(e)=>{
+//         alert("hey the button 1 has been clicked!")
 
-document.querySelector(".box").classList.toggle("harshit");
+// }
+document.querySelector("#Bookmark").addEventListener('click',(e)=>{
+    document.querySelector(".SavedBkmrk").insertAdjacentHTML('beforeend',`<a href=" ${document.querySelector("#enter").value}"class="SavedBkmrk1" target="_blank">${document.querySelector("#enter").value}</a>`)
+    // =document.querySelector("#enter").value
+
+})
