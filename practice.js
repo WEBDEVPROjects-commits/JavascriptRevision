9e9e042ffdd6ea99523b7498d33868772b589874
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-01-17 13:30:04 +0530

    MethodsInPromise

--- practice.js
+++ practice.js
@@ -1,11 +1,22 @@
-        document.querySelector(".Child").addEventListener("click", (e) => {
-            e.stopPropagation();
-            alert("Child was clicked")
-        })
-        document.querySelector(".Childcontainer").addEventListener("click", (e) => {
-            alert("Childcontainer was clicked")
-        })
-        document.querySelector(".Childcontainer").addEventListener("click", (e) => {
-            console.log(e.target);
-            alert("container was clicked")
-        })
+console.log("Hello");
+var prom1 =new Promise((resolve,reject)=>{
+    // var a=Math.random();
+    console.log("console log hoon");
+    reject("Resolve after hoon");
+
+    console.log("console log 1 hoon");
+})
+
+var prom2=new Promise((resolve,reject) => {
+  reject("i am rejecting promise 2");
+})
+
+var prom3=Promise.any([prom1,prom2])
+// var prom3=Promise.allSettled([prom1,prom2])
+// var prom3=Promise.race([prom1,prom2])
+// var prom3=Promise.all([prom1,prom2])
+
+prom3.then((a)=>{
+    console.log(a)
+    console.log(typeof(a))
+})
