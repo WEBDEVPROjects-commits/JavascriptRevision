fc911adc3bd3495f3f37c498325b54ba88f8763f
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-01-17 18:57:57 +0530

    Async,Await

--- practice.js
+++ practice.js
@@ -1,22 +1,15 @@
-console.log("Hello");
-var prom1 =new Promise((resolve,reject)=>{
-    // var a=Math.random();
-    console.log("console log hoon");
-    reject("Resolve after hoon");
+async function getdata(){
+    
+    return new Promise((resolve,reject) => {
+        resolve(1);
+        setTimeout(()=>{
+            console.log("Hello");
+        },0)
+    })
 
-    console.log("console log 1 hoon");
-})
-
-var prom2=new Promise((resolve,reject) => {
-  reject("i am rejecting promise 2");
-})
-
-var prom3=Promise.any([prom1,prom2])
-// var prom3=Promise.allSettled([prom1,prom2])
-// var prom3=Promise.race([prom1,prom2])
-// var prom3=Promise.all([prom1,prom2])
-
-prom3.then((a)=>{
-    console.log(a)
-    console.log(typeof(a))
-})
+}
+async function main(){
+    var data=await getdata();
+    console.log(data);
+}
+main()
