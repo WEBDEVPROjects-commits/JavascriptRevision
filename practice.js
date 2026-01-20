33a09cab096a3ee7f8a47a5a71717bc4b108629b
WEBDEVPROjects-commits <hb7016179@gmail.com>
2026-01-20 17:56:43 +0530

    oopsjs

--- practice.js
+++ practice.js
@@ -1,19 +1,33 @@
-let c="10";
-let a=prompt("Please enter your first number");
-let b=prompt("Please enter your second number");
+class Animal{
+    static a=10;
+    constructor (){
+        console.log("This is animal constructor")
+        console.log(this);
+    }
+    hide(){
+        console.log("animal hides")
+    }
+    run(){
+        console.log("animal can run")
+    }
 
+}
+class rabbit extends Animal{
+    // constructor(){
+    //     console.log("this is rabbit constructor")
+    // }
+    run(){
+        super.run();
+        console.log("rabbit can run")
+        console.log(this.hide())
+    }
 
-if(isNaN(a)||isNaN(b)){
-    throw SyntaxError("This is not allowed");
 }
-let sum=parseInt(a)+parseInt(b)
-
-try{
-    console.log("The sum is "+(10/0));
-}
-catch(error){
-    console.log("error hai ");
-}
-finally{
-    console.log("Finally to chalta hi hai")
-}
+let anm1=new Animal;
+let rb1=new rabbit;
+console.log(anm1.run())
+console.log(rb1.run())
+console.log(Animal.a)
+// console.log(Animal.run())
+console.log()
+console.log(rb1 instanceof rabbit)
